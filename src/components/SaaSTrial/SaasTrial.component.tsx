714d2a3e369dcf0a84edb32527893f9bacf388dc
Harsh Vador <harsh.vador@somaiya.edu>
2024-03-14 12:51:27 +0530

    change background styling

--- src/components/SaaSTrial/SaasTrial.component.tsx
+++ src/components/SaaSTrial/SaasTrial.component.tsx
@@ -31,14 +31,14 @@ const SaasTrial = () => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-full text-light z-0">
+      <div className="svg-border-rounded text-light">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 144.54 17.34"
           preserveAspectRatio="none"
-          className="fill-current"
+          fill="currentColor"
         >
-          <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" />
+          <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path>
         </svg>
       </div>
     </section>
